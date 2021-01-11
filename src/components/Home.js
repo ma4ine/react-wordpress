d3eb4fbd6f2261fbab89f3b3f22f92627f759a3f
Lev N <lev-n@lev-pro.local>
2021-01-11 13:33:08 +0300

    Single post add, and post list fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { Link } from '@reach/router';
 import renderHTML from 'react-render-html';
 import '../Style.css';
 import Moment from 'react-moment';
+import Loader from '../loader.gif';
 
 export class Home extends React.Component {
 
@@ -25,18 +26,19 @@ export class Home extends React.Component {
 				.then( res => {
 					this.setState( { loading: false, posts: res.data });
 				})
-				.catch( error => this.setState({ loading: false, error: error.responce.data }) );
+				.catch( error => this.setState({ loading: false, error: error.responce.data.message }) );
 		});
 	}
 
 	render() {
 
-		const { posts } = this.state;
+		const { posts, loading, error } = this.state;
 		// const posts = this.state.posts; // same thing
 
 		return (
 			<div>
 				<Navbar/>
+				{ error && <div className="alert alert-danger">{ error }</div> }
 				{ posts.length ? (
 					<div className="mt-5 post-container">
 						{ posts.map( post => (
@@ -50,17 +52,19 @@ export class Home extends React.Component {
 								{/* Body */}
 								<div className="card-body">
 									<div className="card-text post-content">
-										{renderHTML(post.content.rendered)}
+										{renderHTML(post.excerpt.rendered)}
 									</div>
 								</div>
 								{/* Footer */}
 								<div className="card-footer">
 									<Moment fromNow>{post.date}</Moment>
+									<Link to={`/post/${post.id}`} className="btn btn-seconsary float-right">Read More...</Link>
 								</div>
 							</div>
 						) ) }
 					</div>
 				) : '' }
+				{ loading && <img className="loader" src={Loader} alt="Loader"/> }
 			</div>
 		)
 	}
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,13 +1,66 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import axios from 'axios';
+import renderHTML from 'react-render-html';
+import '../Style.css';
+import Moment from 'react-moment';
+import Loader from '../loader.gif';
 
 export class SinglePost extends React.Component {
-    render() {
-        return (
-            <div>
-                Single Post
-            </div>
-        )
-    }
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			loading: false,
+			post: {},
+			error: ''
+		}
+	}
+
+	componentDidMount() {
+		const wordPressSiteUrl = 'https://wpreact2.localhost';
+		this.setState( { loading: true }, () => {
+			axios.get( `${wordPressSiteUrl}/wp-json/wp/v2/posts/${this.props.id}` )
+				.then( res => {
+					this.setState( { loading: false, post: res.data });
+				})
+				.catch( error => this.setState({ loading: false, error: error.responce.data.message }) );
+		});
+	}
+
+	render() {
+
+		const { post, error, loading } = this.state;
+
+		return (
+			<div>
+				<Navbar/>
+				{ error && <div className="alert alert-danger">{ error }</div> }
+				{ Object.keys( post ).length ? (
+					<div className="mt-5 post-container">
+							<div key={ post.id } className="card border-dark mb-3" style={{ width: '50rem'}}>
+								{/* Title */}
+								<div className="card-header">
+										{post.title.rendered}
+								</div>
+								{/* Body */}
+								<div className="card-body">
+									<div className="card-text post-content">
+										{renderHTML(post.content.rendered)}
+									</div>
+								</div>
+								{/* Footer */}
+								<div className="card-footer">
+									<Moment fromNow>{post.date}</Moment>
+								</div>
+							</div>
+					</div>
+				) : '' }
+				{ loading && <img className="loader" src={Loader} alt="Loader"/> }
+			</div>
+		)
+	}
 }
 
 export default SinglePost
